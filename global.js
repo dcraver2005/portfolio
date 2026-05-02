65ae3ce0683c915e2c0e98fde1ad2e43fcb297bf
Dylan Craver <dcraver05@gmail.com>
2026-05-01 23:05:35 -0700

    lab 5

--- global.js
+++ global.js
@@ -92,7 +92,10 @@ export function renderProjects(project, containerElement, headingLevel = 'h2') {
     article.innerHTML = `
         <${headingLevel}>${p.title}</${headingLevel}>
         <img src="${p.image}" alt="${p.title}">
-        <p>${p.description}</p>
+        <div>
+          <p>${p.description}</p>
+          <p class="project-year">c. ${p.year}</p>
+        </div>
     `;
     containerElement.appendChild(article);
   }
